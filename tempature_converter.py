ce2db5c83c4e35d60e2eb799589d840850a671d9
Keithd2423 <144353516+Keithd2423@users.noreply.github.com>
2023-11-28 15:13:23 +0000

    message

---
+++ tempature_converter.py
@@ -0,0 +1,15 @@
+print("welcome to tempature converter, a lightweight utility for converting tempatures")
+
+unit = input("please specify the tempature you want converted: ")
+Value = input("please specify the input you want converted: ")
+#function that converts C to F
+def c_2_f(temp_C);
+    converted_C = temp_C * (9/5) + 32
+    return converted_temp
+#function that F to C
+def f_2_C(temp_F);
+    converted_F = (temp_F -32) * 5/9
+    return converted_F
+#main function that uses conditionals to decide which convert function to select
+
+#print result
